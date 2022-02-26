ad8b88a472ccb1a443e53706bec208577d93e3f3
Johnson Ta <Johnson.Ta@bondbrandloyalty.com>
2022-02-26 12:04:07 -0500

    first setup of redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Redux
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import store from './store/store';
+
 import App from './App';
+
+// Styled Components
 import GlobalStyles from './styles/global.styles';
 import { theme } from './styles/theme.styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
